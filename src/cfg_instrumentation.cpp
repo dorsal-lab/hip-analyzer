abbbd330e80e4f8442e523aeb04fb46352b71b35
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-04-25 18:39:59 +0000

    Cfg instrumentation small steps : Replacer stubs

--- src/cfg_instrumentation.cpp
+++ src/cfg_instrumentation.cpp
@@ -7,14 +7,27 @@
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/Analysis/CFG.h"
+#include "clang/Tooling/Core/Replacement.h"
+
+#include "clang/Lex/Lexer.h"
 
 #include <memory>
+#include <sstream>
 #include <string>
+
+// Temporary
+#include <iostream>
 
 using namespace clang;
 using namespace clang::ast_matchers;
 
 namespace hip {
+
+std::string generateBlockCode(unsigned int id) {
+    std::stringstream ss;
+    ss << "// BB" << id << '\n';
+    return ss.str();
+}
 
 /** \class KernelFinder
  * \brief AST Matcher callback to fetch the function text
@@ -33,13 +46,36 @@ class KernelFinder : public MatchFinder::MatchCallback {
             auto lang_opt = Result.Context->getLangOpts();
             cfg->dump(lang_opt, true);
 
+            // Add replacement for counter declaration
+
+            // Add replacement for counter initialization
+
             // Print First elements
             for (auto block : *cfg.get()) {
+                auto id = block->getBlockID();
+
+                std::cout << "\nBlock " << id << '\n';
+
                 auto first = block->front();
                 auto first_statement = first.getAs<clang::CFGStmt>();
 
                 if (first_statement.hasValue()) {
-                    first_statement->getStmt()->dump();
+                    auto stmt = first_statement->getStmt();
+                    auto begin_loc = stmt->getBeginLoc();
+                    begin_loc.dump(*Result.SourceManager);
+
+                    auto rep_loc = clang::Lexer::getLocForEndOfToken(
+                        begin_loc, 0, *Result.SourceManager, lang_opt);
+                    rep_loc.dump(*Result.SourceManager);
+
+                    stmt->dumpColor();
+
+                    // Create replacement
+                    clang::tooling::Replacement rep(*Result.SourceManager,
+                                                    stmt->getBeginLoc(), 0,
+                                                    generateBlockCode(id));
+
+                    std::cout << rep.toString();
                 }
             }
         }
