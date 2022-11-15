be958004c121943730e11c00316ae0bd930b905e
Sebastien Darche <sebastien.darche@polymtl.ca>
2022-11-15 16:16:20 -0500

    Transition to new PM part2 : registration to pass manager

--- src/pass/hip_analyzer_pass.cpp
+++ src/pass/hip_analyzer_pass.cpp
@@ -695,3 +695,36 @@ llvm::AnalysisKey AnalysisPass::Key;
 } // namespace
 
 } // namespace hip
+
+llvm::PassPluginLibraryInfo getHipAnalyzerPluginInfo() {
+    return {
+        LLVM_PLUGIN_API_VERSION, "hip-analyzer", LLVM_VERSION_STRING,
+        [](llvm::PassBuilder& pb) {
+            pb.registerPipelineParsingCallback(
+                [&](llvm::StringRef name, llvm::ModulePassManager& mpm,
+                    llvm::ArrayRef<llvm::PassBuilder::PipelineElement>) {
+                    if (name == "hip-analyzer-host") {
+                        mpm.addPass(hip::HostPass());
+                        return true;
+                    } else if (name == "hip-analyzer-counters") {
+                        mpm.addPass(hip::CfgInstrumentationPass());
+                        return true;
+                    }
+                    return false;
+                });
+
+            pb.registerPipelineStartEPCallback(
+                [](llvm::ModulePassManager& pm, llvm::OptimizationLevel Level) {
+                    pm.addPass(hip::HostPass());
+                });
+            pb.registerAnalysisRegistrationCallback(
+                [](llvm::FunctionAnalysisManager& fam) {
+                    fam.registerPass([&] { return hip::AnalysisPass(); });
+                });
+        }};
+}
+
+extern "C" LLVM_ATTRIBUTE_WEAK ::llvm::PassPluginLibraryInfo
+llvmGetPassPluginInfo() {
+    return getHipAnalyzerPluginInfo();
+}
