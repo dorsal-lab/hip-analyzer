d740c61bcf6440bef7ba5df782ffbe27c1c1745c
Sebastien Darche <sebastien.darche@polymtl.ca>
2024-05-03 14:00:52 -0400

    Prepare sample for tests

--- samples/square.cpp
+++ samples/square.cpp
@@ -1,53 +1,64 @@
-/*
-Copyright (c) 2015 - 2021 Advanced Micro Devices, Inc. All rights reserved.
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
 #include "hip/hip_runtime.h"
-#include <stdio.h>
+
+#include <charconv>
+#include <iostream>
+#include <optional>
+
+#include <execinfo.h>
+#include <unistd.h>
+
+inline void check(hipError_t err) {
+    if (err != hipSuccess) {
+        std::cerr << "error : " << hipGetErrorString(err) << " (" << err
+                  << ")\nBacktrace :\n";
+
+        void* fds[32];
+        size_t size;
+
+        size = backtrace(fds, 32);
+        backtrace_symbols_fd(fds, size, STDERR_FILENO);
+
+        throw std::runtime_error(std::string("Encountered hip error ") +
+                                 hipGetErrorString(err));
+    }
+}
 
 /*
  * Square each element in the array A and write to array C.
  */
 template <typename T>
 __global__ void vector_square(T* C_d, const T* A_d, size_t N) {
-    size_t offset = (hipBlockIdx_x * hipBlockDim_x + hipThreadIdx_x);
-    size_t stride = hipBlockDim_x * hipGridDim_x;
+    size_t offset = (blockIdx.x * blockDim.x + threadIdx.x);
+    size_t stride = blockDim.x * gridDim.x;
 
     for (size_t i = offset; i < N; i += stride) {
         C_d[i] = A_d[i] * A_d[i];
     }
 }
 
+std::optional<uint64_t> parse_env(const std::string& env) {
+    auto buffer_size = std::getenv(env.c_str());
+    if (buffer_size == nullptr) {
+        return std::nullopt;
+    }
+
+    uint64_t val;
+    std::string str{buffer_size};
+    if (std::from_chars(str.data(), str.data() + str.length(), val).ec !=
+        std::errc()) {
+        std::cerr << "Could not parse u64 token : " << str << '\n';
+        return std::nullopt;
+    }
+
+    return val;
+}
+
 int main(int argc, char* argv[]) {
     float *A_d, *C_d;
     float *A_h, *C_h;
-    size_t N = 1000000;
+    size_t N = parse_env("SQUARE_SIZE").value_or(8182);
     size_t Nbytes = N * sizeof(float);
-    static int device = 1;
-    hipSetDevice(device);
-    hipDeviceProp_t props;
-    hipGetDeviceProperties(&props, device /*deviceID*/);
-    printf("info: running on device %s\n", props.name);
-#ifdef __HIP_PLATFORM_AMD__
-    printf("info: architecture on AMD GPU device is: %s\n", props.gcnArchName);
-#endif
-    printf("info: allocate host mem (%6.2f MB)\n",
-           2 * Nbytes / 1024.0 / 1024.0);
+
     A_h = (float*)malloc(Nbytes);
 
     C_h = (float*)malloc(Nbytes);
@@ -57,29 +68,22 @@ int main(int argc, char* argv[]) {
         A_h[i] = 1.618f + i;
     }
 
-    printf("info: allocate device mem (%6.2f MB)\n",
-           2 * Nbytes / 1024.0 / 1024.0);
-    hipMalloc(&A_d, Nbytes);
-    hipMalloc(&C_d, Nbytes);
+    check(hipMalloc(&A_d, Nbytes));
+    check(hipMalloc(&C_d, Nbytes));
 
-    printf("info: copy Host2Device\n");
-    hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice);
+    check(hipMemcpy(A_d, A_h, Nbytes, hipMemcpyHostToDevice));
 
-    const unsigned blocks = 512;
-    const unsigned threadsPerBlock = 256;
+    const unsigned blocks = parse_env("SQUARE_TOTAL_SIZE").value_or(512);
+    const unsigned threadsPerBlock = parse_env("SQUARE_WG_SIZE").value_or(256);
 
-    printf("info: launch 'vector_square' kernel\n");
-    hipLaunchKernelGGL(vector_square, dim3(blocks), dim3(threadsPerBlock), 0, 0,
-                       C_d, A_d, N);
+    const auto nb_it = parse_env("SQUARE_NB_IT").value_or(1);
 
-    printf("info: copy Device2Host\n");
-    hipMemcpy(C_h, C_d, Nbytes, hipMemcpyDeviceToHost);
+    for (auto i = 0u; i < nb_it; ++i) {
+        vector_square<<<dim3(blocks), dim3(threadsPerBlock)>>>(C_d, A_d, N);
 
-    printf("info: check result\n");
-    for (size_t i = 0; i < N; i++) {
-        if (C_h[i] != A_h[i] * A_h[i]) {
-            throw std::runtime_error("");
-        }
+        check(hipDeviceSynchronize());
+        check(hipMemcpy(A_d, C_d, Nbytes, hipMemcpyHostToHost));
     }
-    printf("PASSED!\n");
+
+    check(hipMemcpy(C_h, C_d, Nbytes, hipMemcpyDeviceToHost));
 }
